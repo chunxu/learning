c8c672cc64990e9754881c3f5a0dba5a4ecd5a7c
chunxu <41453752+chunxu@users.noreply.github.com>
2022-02-21 23:21:46 -0600

    Create StatisticalTests.py

---
+++ StatisticalTests.py
@@ -0,0 +1,107 @@
+#Statistical Hypothesis Tests
+
+
+# Example of the Student's t-test
+from scipy.stats import ttest_ind
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+stat, p = ttest_ind(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+
+
+
+
+# Example of the Paired Student's t-test
+from scipy.stats import ttest_rel
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+stat, p = ttest_rel(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+
+
+
+
+# Example of the Analysis of Variance Test ANOVA
+from scipy.stats import f_oneway
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+data3 = [-0.208, 0.696, 0.928, -1.148, -0.213, 0.229, 0.137, 0.269, -0.870, -1.204]
+stat, p = f_oneway(data1, data2, data3)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+
+
+
+
+
+# Example of the Mann-Whitney U Test
+from scipy.stats import mannwhitneyu
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+stat, p = mannwhitneyu(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+
+
+
+
+
+# Example of the Wilcoxon Signed-Rank Test
+from scipy.stats import wilcoxon
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+stat, p = wilcoxon(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+
+
+
+
+
+# Example of the Kruskal-Wallis H Test
+from scipy.stats import kruskal
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+stat, p = kruskal(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+
+
+
+
+# Example of the Friedman Test
+from scipy.stats import friedmanchisquare
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [1.142, -0.432, -0.938, -0.729, -0.846, -0.157, 0.500, 1.183, -1.075, -0.169]
+data3 = [-0.208, 0.696, 0.928, -1.148, -0.213, 0.229, 0.137, 0.269, -0.870, -1.204]
+stat, p = friedmanchisquare(data1, data2, data3)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably the same distribution')
+else:
+	print('Probably different distributions')
+
+
+
+
+
