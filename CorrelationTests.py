b36bbba38399a0a817d2e91079d526c3a5044aaa
chunxu <41453752+chunxu@users.noreply.github.com>
2022-02-21 23:21:04 -0600

    Create CorrelationTests.py

---
+++ CorrelationTests.py
@@ -0,0 +1,60 @@
+#Correlation Tests
+
+# Example of the Pearson's Correlation test
+from scipy.stats import pearsonr
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [0.353, 3.517, 0.125, -7.545, -0.555, -1.536, 3.350, -1.578, -3.537, -1.579]
+stat, p = pearsonr(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably independent')
+else:
+	print('Probably dependent')
+
+
+
+
+
+
+# Example of the Spearman's Rank Correlation Test
+from scipy.stats import spearmanr
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [0.353, 3.517, 0.125, -7.545, -0.555, -1.536, 3.350, -1.578, -3.537, -1.579]
+stat, p = spearmanr(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably independent')
+else:
+	print('Probably dependent')
+
+
+
+
+# Example of the Kendall's Rank Correlation Test
+from scipy.stats import kendalltau
+data1 = [0.873, 2.817, 0.121, -0.945, -0.055, -1.436, 0.360, -1.478, -1.637, -1.869]
+data2 = [0.353, 3.517, 0.125, -7.545, -0.555, -1.536, 3.350, -1.578, -3.537, -1.579]
+stat, p = kendalltau(data1, data2)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably independent')
+else:
+	print('Probably dependent')
+
+
+
+
+# Example of the Chi-Squared Test
+from scipy.stats import chi2_contingency
+table = [[10, 20, 30],[6,  9,  17]]
+stat, p, dof, expected = chi2_contingency(table)
+print('stat=%.3f, p=%.3f' % (stat, p))
+if p > 0.05:
+	print('Probably independent')
+else:
+	print('Probably dependent')
+
+
+
+
+
