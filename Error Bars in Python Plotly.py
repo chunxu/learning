021e22184d171072c13c50767aa8cab83157fc58
chunxu <41453752+chunxu@users.noreply.github.com>
2022-03-21 11:45:08 -0500

    add #Asymmetric Error Bars with Plotly Express

--- Error Bars in Python Plotly.py
+++ Error Bars in Python Plotly.py
@@ -12,3 +12,13 @@ fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
 fig.show()
 
 
+
+#Asymmetric Error Bars with Plotly Express
+import plotly.express as px
+df = px.data.iris()
+df["e_plus"] = df["sepal_width"]/100
+df["e_minus"] = df["sepal_width"]/40
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
+                 error_y="e_plus", error_y_minus="e_minus")
+fig.show()
+
