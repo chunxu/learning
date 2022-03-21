96c4562fd2af6d36e97fb0dbe2003bd62d8da1ba
chunxu <41453752+chunxu@users.noreply.github.com>
2022-03-21 11:44:23 -0500

    Update Error Bars in Python Plotly.py

--- Error Bars in Python Plotly.py
+++ Error Bars in Python Plotly.py
@@ -1,1 +1,14 @@
+#Error Bars in Python Plotly 
+
+'''
+Plotly Express is the easy-to-use, high-level interface to Plotly, which operates on a variety of types of data and produces easy-to-style figures. For functions representing 2D data points such as px.scatter, px.line, px.bar etc., error bars are given as a column name which is the value of the error_x (for the error on x position) and error_y (for the error on y position).
+'''
+
+import plotly.express as px
+df = px.data.iris()
+df["e"] = df["sepal_width"]/100
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
+                 error_x="e", error_y="e")
+fig.show()
+
 
