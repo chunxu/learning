265e940cac1d0e0cf0d9e55fbe7b70fc63941edc
chunxu <41453752+chunxu@users.noreply.github.com>
2022-03-22 21:57:05 -0500

    Create house_price_prediction.py

---
+++ house_price_prediction.py
@@ -0,0 +1,12 @@
+#oringinally drafted in jupter notebook
+import numpy as np
+import pandas as pd
+#pip install plotly
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.io as pio
+pio.templates
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline
