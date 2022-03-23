e99a576525180b659e22f1c8c1c111d2c3106863
chunxu <41453752+chunxu@users.noreply.github.com>
2022-03-22 21:57:42 -0500

    load data

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -10,3 +10,12 @@ pio.templates
 import seaborn as sns
 import matplotlib.pyplot as plt
 %matplotlib inline
+
+from sklearn.datasets import load_boston
+load_boston = load_boston()
+x = load_boston.data
+y = load_boston.target
+
+data = pd.DataFrame(x, columns = load_boston.feature_names)
+data['SalePrice'] = y
+data.head()
