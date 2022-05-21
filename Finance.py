0fb0677dcfe6e8182fd9431272915ef99fa979d7
chunxu <41453752+chunxu@users.noreply.github.com>
2022-05-21 07:42:16 -0500

    Update Finance.py

--- Finance.py
+++ Finance.py
@@ -13,3 +13,77 @@
 # ** NOTE: This project is extremely challenging because it will introduce a lot of new concepts and have you looking things up on your own (we'll point you in the right direction) to try to solve the tasks issued. Feel free to just go through the solutions lecture notebook and video as a "walkthrough" project if you don't want to have to look things up yourself. You'll still learn a lot that way! **
 # ____
 # We'll focus on bank stocks and see how they progressed throughout the [financial crisis](https://en.wikipedia.org/wiki/Financial_crisis_of_2007%E2%80%9308) all the way to early 2016.
+
+# ## Get the Data
+# 
+# In this section we will learn how to use pandas to directly read data from Google finance using pandas!
+# 
+# First we need to start with the proper imports, which we've already laid out for you here.
+# 
+# *Note: [You'll need to install pandas-datareader for this to work!](https://github.com/pydata/pandas-datareader) Pandas datareader allows you to [read stock information directly from the internet](http://pandas.pydata.org/pandas-docs/stable/remote_data.html) Use these links for install guidance (**pip install pandas-datareader**), or just follow along with the video lecture.*
+# 
+# ### The Imports
+# 
+# Already filled out for you.
+
+#pip install pandas-datareader
+
+from pandas_datareader import data, wb
+import pandas as pd
+import numpy as np
+import datetime
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# ## Data
+# 
+# We need to get data using pandas datareader. We will get stock information for the following banks:
+# *  Bank of America
+# * CitiGroup
+# * Goldman Sachs
+# * JPMorgan Chase
+# * Morgan Stanley
+# * Wells Fargo
+# 
+# ** Figure out how to get the stock data from Jan 1st 2006 to Jan 1st 2016 for each of these banks. Set each bank to be a separate dataframe, with the variable name for that bank being its ticker symbol. This will involve a few steps:**
+# 1. Use datetime to set start and end datetime objects.
+# 2. Figure out the ticker symbol for each bank.
+# 2. Figure out how to use datareader to grab info on the stock.
+# 
+# ** Use [this documentation page](https://pandas-datareader.readthedocs.io/en/latest/remote_data.html) for hints and instructions (it should just be a matter of replacing certain values. Use google finance as a source, for example:**
+#     
+#     # Bank of America
+#     BAC = data.DataReader("BAC", 'google', start, end)
+# 
+# ### WARNING: MAKE SURE TO CHECK THE LINK ABOVE FOR THE LATEST WORKING API. "google" MAY NOT ALWAYS WORK. 
+# ------------
+# ### We also provide pickle file in the article lecture right before the video lectures.
+
+
+#df = pd.read_pickle('all_banks')
+
+
+start = datetime.datetime(2006,1,1)
+end = datetime.datetime(2016,1,1)
+
+
+BAC = data.DataReader('BAC','yahoo', start, end)
+C = data.DataReader('C','yahoo', start, end)
+
+
+# ** Create a list of the ticker symbols (as strings) in alphabetical order. Call this list: tickers**
+
+tickers = ['BAC','C']
+
+
+# ** Use pd.concat to concatenate the bank dataframes together to a single data frame called bank_stocks. Set the keys argument equal to the tickers list. Also pay attention to what axis you concatenate on.**
+
+bank_stocks = pd.concat([BAC,C], axis=1, keys = tickers)
+
+# ** Set the column name levels (this is filled out for you):**
+
+bank_stocks.columns.names = ['Bank Ticker','Stock Info']
+
+# ** Check the head of the bank_stocks dataframe.**
+
+bank_stocks.head(2)
