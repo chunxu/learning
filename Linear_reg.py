10b0b92c99a8439d76f42c3c451a47bfd41f170a
chunxu <41453752+chunxu@users.noreply.github.com>
2022-05-30 19:40:12 -0500

    Update Linear_reg.py

--- Linear_reg.py
+++ Linear_reg.py
@@ -29,3 +29,49 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 get_ipython().run_line_magic('matplotlib', 'inline')
 
+
+USAhousing = pd.read_csv('USA_Housing.csv')
+
+USAhousing.head()
+
+USAhousing.info()
+
+USAhousing.describe()
+
+USAhousing.columns
+
+sns.pairplot(USAhousing)
+
+sns.distplot(USAhousing['Price'])
+
+sns.heatmap(USAhousing.corr())
+
+# ## Training a Linear Regression Model
+# 
+# Let's now begin to train out regression model! We will need to first split up our data into an X array that contains the features to train on, and a y array with the target variable, in this case the Price column. We will toss out the Address column because it only has text info that the linear regression model can't use.
+# 
+# ### X and y arrays
+
+X = USAhousing[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+               'Avg. Area Number of Bedrooms', 'Area Population']]
+y = USAhousing['Price']
+
+
+# ## Train Test Split
+# 
+# Now let's split the data into a training set and a testing set. We will train out model on the training set and then use the test set to evaluate the model.
+
+from sklearn.model_selection import train_test_split
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+
+
+# ## Creating and Training the Model
+
+from sklearn.linear_model import LinearRegression
+
+lm = LinearRegression()
+
+lm.fit(X_train,y_train)
+
