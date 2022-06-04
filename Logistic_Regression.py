7b60871f517de13dda80b7949588eaa03e16dc47
chunxu <41453752+chunxu@users.noreply.github.com>
2022-06-03 21:17:30 -0500

    Create Logistic_Regression.py

---
+++ Logistic_Regression.py
@@ -0,0 +1,51 @@
+# # Logistic Regression with Python
+# 
+# For this lecture we will be working with the [Titanic Data Set from Kaggle](https://www.kaggle.com/c/titanic). This is a very famous data set and very often is a student's first step in machine learning! 
+# 
+# We'll be trying to predict a classification- survival or deceased.
+# Let's begin our understanding of implementing Logistic Regression in Python for classification.
+# 
+# We'll use a "semi-cleaned" version of the titanic data set, if you use the data set hosted directly on Kaggle, you may need to do some additional cleaning not shown in this lecture notebook.
+# 
+# ## Import Libraries
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+train = pd.read_csv('titanic_train.csv')
+
+train.head()
+
+# ## Missing Data
+# 
+# We can use seaborn to create a simple heatmap to see where we are missing data!
+
+sns.heatmap(train.isnull(),yticklabels=False,cbar=False,cmap='viridis')
+
+sns.set_style('whitegrid')
+sns.countplot(x='Survived',data=train,palette='RdBu_r')
+
+sns.set_style('whitegrid')
+sns.countplot(x='Survived',hue='Sex',data=train,palette='RdBu_r')
+
+sns.set_style('whitegrid')
+sns.countplot(x='Survived',hue='Pclass',data=train,palette='rainbow')
+
+sns.distplot(train['Age'].dropna(),kde=False,color='darkred',bins=30)
+
+train['Age'].hist(bins=30,color='darkred',alpha=0.7)
+
+sns.countplot(x='SibSp',data=train)
+
+train['Fare'].hist(color='green',bins=40,figsize=(8,4))
+# ### Cufflinks for plots
+
+
+import cufflinks as cf
+cf.go_offline()
+
+train['Fare'].iplot(kind='hist',bins=30,color='green')
