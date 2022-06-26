8bbe576220e076aa8ed4577f6a5d0ebfe884b8d1
chunxu <41453752+chunxu@users.noreply.github.com>
2022-06-26 10:44:38 -0500

    Create K Nearest Neighbors.py

---
+++ K Nearest Neighbors.py
@@ -0,0 +1,23 @@
+# You've been given a classified data set from a company! They've hidden the feature column names but have given you the data and the target classes. 
+# 
+# We'll try to use KNN to create a model that directly predicts a class for a new data point based off of the features.
+# 
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+df = pd.read_csv("Classified Data",index_col=0)
+
+# ## Standardize the Variables
+# 
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+scaler.fit(df.drop('TARGET CLASS',axis=1))
+
+scaled_features = scaler.transform(df.drop('TARGET CLASS',axis=1))
+
+df_feat = pd.DataFrame(scaled_features,columns=df.columns[:-1])
+df_feat.head()
