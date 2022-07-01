71bf1ebc1bfdad7c6177c5271fe8b10d8dfbc70c
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-01 11:03:49 -0500

    Update RecommenderSystems.py

--- RecommenderSystems.py
+++ RecommenderSystems.py
@@ -41,3 +41,43 @@ plt.figure(figsize=(10,4))
 ratings['rating'].hist(bins=70)
 
 sns.jointplot(x='rating',y='num of ratings',data=ratings,alpha=0.5)
+
+# ## Recommending Similar Movies
+
+moviemat = df.pivot_table(index='user_id',columns='title',values='rating')
+moviemat.head()
+
+ratings.sort_values('num of ratings',ascending=False).head(10)
+
+starwars_user_ratings = moviemat['Star Wars (1977)']
+liarliar_user_ratings = moviemat['Liar Liar (1997)']
+starwars_user_ratings.head()
+
+
+# We can then use corrwith() method to get correlations between two pandas series:
+
+similar_to_starwars = moviemat.corrwith(starwars_user_ratings)
+similar_to_liarliar = moviemat.corrwith(liarliar_user_ratings)
+
+corr_starwars = pd.DataFrame(similar_to_starwars,columns=['Correlation'])
+corr_starwars.dropna(inplace=True)
+corr_starwars.head()
+
+# Now if we sort the dataframe by correlation, we should get the most similar movies, however note that we get some results that don't really make sense. This is because there are a lot of movies only watched once by users who also watched star wars (it was the most popular movie). 
+
+corr_starwars.sort_values('Correlation',ascending=False).head(10)
+
+corr_liarliar.sort_values('Correlation',ascending=False).head(10)
+
+
+# Let's fix this by filtering out movies that have less than 100 reviews (this value was chosen based off the histogram from earlier).
+
+corr_starwars = corr_starwars.join(ratings['num of ratings'])
+corr_starwars.head()
+
+corr_starwars[corr_starwars['num of ratings']>100].sort_values('Correlation',ascending=False).head()
+
+corr_liarliar = pd.DataFrame(similar_to_liarliar,columns=['Correlation'])
+corr_liarliar.dropna(inplace=True)
+corr_liarliar = corr_liarliar.join(ratings['num of ratings'])
+corr_liarliar[corr_liarliar['num of ratings']>100].sort_values('Correlation',ascending=False).head()
