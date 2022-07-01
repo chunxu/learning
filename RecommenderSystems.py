4c867b656b659ca541c69568f3cc1feea2cccaa0
chunxu <41453752+chunxu@users.noreply.github.com>
2022-07-01 11:01:32 -0500

    Update RecommenderSystems.py

--- RecommenderSystems.py
+++ RecommenderSystems.py
@@ -4,3 +4,31 @@
 # In this notebook, we will focus on providing a basic recommendation system by suggesting items that are most similar to a particular item, in this case, movies. Keep in mind, this is not a true robust recommendation system, to describe it more accurately,it just tells you what movies/items are most similar to your movie choice.
 # 
 
+
+import numpy as np
+import pandas as pd
+
+column_names = ['user_id', 'item_id', 'rating', 'timestamp']
+df = pd.read_csv('u.data', sep='\t', names=column_names)
+
+movie_titles = pd.read_csv("Movie_Id_Titles")
+
+df = pd.merge(df,movie_titles,on='item_id')
+df.head()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('white')
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+
+df.groupby('title')['rating'].mean().sort_values(ascending=False).head()
+
+df.groupby('title')['rating'].count().sort_values(ascending=False).head()
+
+ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
+ratings.head()
+
+ratings['num of ratings'] = pd.DataFrame(df.groupby('title')['rating'].count())
+ratings.head()
