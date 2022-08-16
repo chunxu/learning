7ce94cc4a6bdff76d1d5586854f4188996c36f25
chunxu <41453752+chunxu@users.noreply.github.com>
2022-08-16 09:39:40 -0500

    Update Principal Component Analysis.py

--- Principal Component Analysis.py
+++ Principal Component Analysis.py
@@ -19,7 +19,6 @@
 
 # ## Libraries
 
-# In[1]:
 
 
 import matplotlib.pyplot as plt
@@ -33,39 +32,24 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # 
 # Let's work with the cancer data set again since it had so many features.
 
-# In[2]:
 
 
 from sklearn.datasets import load_breast_cancer
 
 
-# In[3]:
-
-
 cancer = load_breast_cancer()
 
-
-# In[4]:
 
 
 cancer.keys()
 
 
-# In[5]:
-
-
 print(cancer['DESCR'])
 
-
-# In[6]:
 
 
 df = pd.DataFrame(cancer['data'],columns=cancer['feature_names'])
 #(['DESCR', 'data', 'feature_names', 'target_names', 'target'])
-
-
-# In[7]:
-
 
 df.head()
 
@@ -74,21 +58,12 @@ df.head()
 # 
 # As we've noticed before it is difficult to visualize high dimensional data, we can use PCA to find the first two principal components, and visualize the data in this new, two-dimensional space, with a single scatter-plot. Before we do this though, we'll need to scale our data so that each feature has a single unit variance.
 
-# In[8]:
-
 
 from sklearn.preprocessing import StandardScaler
 
 
-# In[9]:
-
-
 scaler = StandardScaler()
 scaler.fit(df)
-
-
-# In[10]:
-
 
 scaled_data = scaler.transform(df)
 
@@ -97,39 +72,23 @@ scaled_data = scaler.transform(df)
 # 
 # We can also specify how many components we want to keep when creating the PCA object.
 
-# In[11]:
-
 
 from sklearn.decomposition import PCA
 
 
-# In[29]:
-
-
 pca = PCA(n_components=2)
-
-
-# In[30]:
-
 
 pca.fit(scaled_data)
 
 
 # Now we can transform this data to its first 2 principal components.
 
-# In[31]:
-
 
 x_pca = pca.transform(scaled_data)
 
 
-# In[32]:
-
 
 scaled_data.shape
-
-
-# In[33]:
 
 
 x_pca.shape
@@ -137,7 +96,6 @@ x_pca.shape
 
 # Great! We've reduced 30 dimensions to just 2! Let's plot these two dimensions out!
 
-# In[36]:
 
 
 fig = plt.figure()
@@ -148,8 +106,6 @@ xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
 ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
 ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens');
 
-
-# In[17]:
 
 
 plt.figure(figsize=(8,6))
@@ -166,31 +122,16 @@ plt.ylabel('Second Principal Component')
 # 
 # The components correspond to combinations of the original features, the components themselves are stored as an attribute of the fitted PCA object:
 
-# In[18]:
-
-
 pca.components_
 
 
 # In this numpy matrix array, each row represents a principal component, and each column relates back to the original features. we can visualize this relationship with a heatmap:
 
-# In[19]:
-
 
 df_comp = pd.DataFrame(pca.components_,columns=cancer['feature_names'])
-
-
-# In[20]:
 
 
 plt.figure(figsize=(12,6))
 sns.heatmap(df_comp,cmap='plasma',)
 
 
-# This heatmap and the color bar basically represent the correlation between the various feature and the principal component itself.
-# 
-# ## Conclusion
-# 
-# Hopefully this information is useful to you when dealing with high dimensional data!
-
-# # Great Job!
