64ebf349501249ac591f839813efc8b671f8f23d
chunxu <41453752+chunxu@users.noreply.github.com>
2022-08-20 20:42:53 -0500

    Update Principal Component Analysis.py

--- Principal Component Analysis.py
+++ Principal Component Analysis.py
@@ -17,77 +17,52 @@
 
 # ## Libraries
 
-
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import seaborn as sns
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# ## The Data
 # Let's work with the cancer data set again since it had so many features.
 
 from sklearn.datasets import load_breast_cancer
 
 cancer = load_breast_cancer()
-
 cancer.keys()
-
 print(cancer['DESCR'])
-
-
 
 df = pd.DataFrame(cancer['data'],columns=cancer['feature_names'])
 #(['DESCR', 'data', 'feature_names', 'target_names', 'target'])
-
 df.head()
 
-
 # ## PCA Visualization
-# 
 # As we've noticed before it is difficult to visualize high dimensional data, we can use PCA to find the first two principal components, and visualize the data in this new, two-dimensional space, with a single scatter-plot. Before we do this though, we'll need to scale our data so that each feature has a single unit variance.
-
-
 from sklearn.preprocessing import StandardScaler
-
-
 scaler = StandardScaler()
 scaler.fit(df)
 
 scaled_data = scaler.transform(df)
 
-
 # PCA with Scikit Learn uses a very similar process to other preprocessing functions that come with SciKit Learn. We instantiate a PCA object, find the principal components using the fit method, then apply the rotation and dimensionality reduction by calling transform().
 # 
 # We can also specify how many components we want to keep when creating the PCA object.
 
-
 from sklearn.decomposition import PCA
-
 
 pca = PCA(n_components=2)
 
 pca.fit(scaled_data)
 
-
 # Now we can transform this data to its first 2 principal components.
-
 
 x_pca = pca.transform(scaled_data)
 
-
-
 scaled_data.shape
-
 
 x_pca.shape
 
 
 # Great! We've reduced 30 dimensions to just 2! Let's plot these two dimensions out!
-
-
 
 fig = plt.figure()
 plt.figure(figsize=(12,10))
@@ -97,13 +72,10 @@ xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
 ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
 ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens');
 
-
-
 plt.figure(figsize=(8,6))
 plt.scatter(x_pca[:,0],x_pca[:,1],c=cancer['target'],cmap='plasma')
 plt.xlabel('First principal component')
 plt.ylabel('Second Principal Component')
-
 
 # Clearly by using these two components we can easily separate these two classes.
 # 
