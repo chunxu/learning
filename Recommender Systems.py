7691a60e0950e24c5e1ac50265d484532832d483
chunxu <41453752+chunxu@users.noreply.github.com>
2022-08-22 21:15:42 -0500

    Update Recommender Systems.py

--- Recommender Systems.py
+++ Recommender Systems.py
@@ -60,22 +60,13 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 df.groupby('title')['rating'].mean().sort_values(ascending=False).head()
 
 
-# In[10]:
-
-
 df.groupby('title')['rating'].count().sort_values(ascending=False).head()
-
-
-# In[11]:
-
 
 ratings = pd.DataFrame(df.groupby('title')['rating'].mean())
 ratings.head()
 
 
 # Now set the number of ratings column:
-
-# In[12]:
 
 
 ratings['num of ratings'] = pd.DataFrame(df.groupby('title')['rating'].count())
@@ -84,22 +75,11 @@ ratings.head()
 
 # Now a few histograms:
 
-# In[13]:
-
-
 plt.figure(figsize=(10,4))
 ratings['num of ratings'].hist(bins=70)
 
-
-# In[14]:
-
-
 plt.figure(figsize=(10,4))
 ratings['rating'].hist(bins=70)
-
-
-# In[15]:
-
 
 sns.jointplot(x='rating',y='num of ratings',data=ratings,alpha=0.5)
 
@@ -110,16 +90,11 @@ sns.jointplot(x='rating',y='num of ratings',data=ratings,alpha=0.5)
 
 # Now let's create a matrix that has the user ids on one access and the movie title on another axis. Each cell will then consist of the rating the user gave to that movie. Note there will be a lot of NaN values, because most people have not seen most of the movies.
 
-# In[16]:
-
-
 moviemat = df.pivot_table(index='user_id',columns='title',values='rating')
 moviemat.head()
 
 
 # Most rated movie:
-
-# In[17]:
 
 
 ratings.sort_values('num of ratings',ascending=False).head(10)
@@ -127,16 +102,10 @@ ratings.sort_values('num of ratings',ascending=False).head(10)
 
 # Let's choose two movies: starwars, a sci-fi movie. And Liar Liar, a comedy.
 
-# In[18]:
-
-
 ratings.head()
 
 
 # Now let's grab the user ratings for those two movies:
-
-# In[19]:
-
 
 starwars_user_ratings = moviemat['Star Wars (1977)']
 liarliar_user_ratings = moviemat['Liar Liar (1997)']
@@ -145,22 +114,13 @@ starwars_user_ratings.head()
 
 # We can then use corrwith() method to get correlations between two pandas series:
 
-# In[20]:
-
-
 similar_to_starwars = moviemat.corrwith(starwars_user_ratings)
 similar_to_liarliar = moviemat.corrwith(liarliar_user_ratings)
 
 
 # Let's clean this by removing NaN values and using a DataFrame instead of a series:
 
-# In[22]:
-
-
 similar_to_liarliar
-
-
-# In[23]:
 
 
 corr_starwars = pd.DataFrame(similar_to_starwars,columns=['Correlation'])
@@ -170,21 +130,13 @@ corr_starwars.head()
 
 # Now if we sort the dataframe by correlation, we should get the most similar movies, however note that we get some results that don't really make sense. This is because there are a lot of movies only watched once by users who also watched star wars (it was the most popular movie). 
 
-# In[24]:
-
-
 corr_starwars.sort_values('Correlation',ascending=False).head(10)
-
-
-# In[28]:
 
 
 corr_liarliar.sort_values('Correlation',ascending=False).head(10)
 
 
 # Let's fix this by filtering out movies that have less than 100 reviews (this value was chosen based off the histogram from earlier).
-
-# In[25]:
 
 
 corr_starwars = corr_starwars.join(ratings['num of ratings'])
@@ -193,15 +145,11 @@ corr_starwars.head()
 
 # Now sort the values and notice how the titles make a lot more sense:
 
-# In[26]:
-
 
 corr_starwars[corr_starwars['num of ratings']>100].sort_values('Correlation',ascending=False).head()
 
 
 # Now the same for the comedy Liar Liar:
-
-# In[27]:
 
 
 corr_liarliar = pd.DataFrame(similar_to_liarliar,columns=['Correlation'])
@@ -210,4 +158,3 @@ corr_liarliar = corr_liarliar.join(ratings['num of ratings'])
 corr_liarliar[corr_liarliar['num of ratings']>100].sort_values('Correlation',ascending=False).head()
 
 
-# # Great Job!
