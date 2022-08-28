b3d66afe0747419961d28613dd85a319421878d0
chunxu <41453752+chunxu@users.noreply.github.com>
2022-08-28 12:20:39 -0500

    Update Tensorflow Basics.py

--- Tensorflow Basics.py
+++ Tensorflow Basics.py
@@ -11,13 +11,8 @@
 # 
 # You can import the library:
 
-# In[28]:
-
 
 import tensorflow as tf
-
-
-# In[29]:
 
 
 print(tf.__version__)
@@ -27,25 +22,13 @@ print(tf.__version__)
 # 
 # Let's show how to create a simple constant with Tensorflow, which TF stores as a tensor object:
 
-# In[2]:
-
 
 hello = tf.constant('Hello World')
 
 
-# In[3]:
-
-
 type(hello)
 
-
-# In[4]:
-
-
 x = tf.constant(100)
-
-
-# In[5]:
 
 
 type(x)
@@ -58,32 +41,13 @@ type(x)
 # A `Session` object encapsulates the environment in which `Operation`
 # objects are executed, and `Tensor` objects are evaluated. For example:
 
-# In[6]:
-
-
 sess = tf.Session()
-
-
-# In[7]:
-
 
 sess.run(hello)
 
-
-# In[8]:
-
-
 type(sess.run(hello))
 
-
-# In[9]:
-
-
 sess.run(x)
-
-
-# In[10]:
-
 
 type(sess.run(x))
 
@@ -92,14 +56,9 @@ type(sess.run(x))
 # 
 # You can line up multiple Tensorflow operations in to be run during a session:
 
-# In[11]:
-
 
 x = tf.constant(2)
 y = tf.constant(3)
-
-
-# In[12]:
 
 
 with tf.Session() as sess:
@@ -121,29 +80,13 @@ with tf.Session() as sess:
 # 
 # Here is an example for integer placeholders:
 
-# In[13]:
-
-
 x = tf.placeholder(tf.int32)
 y = tf.placeholder(tf.int32)
-
-
-# In[14]:
-
-
 x
-
-
-# In[15]:
-
-
 type(x)
 
 
 # ### Defining Operations
-
-# In[16]:
-
 
 add = tf.add(x,y)
 sub = tf.subtract(x,y)
@@ -152,14 +95,8 @@ mul = tf.multiply(x,y)
 
 # Running operations with variable input:
 
-# In[17]:
-
 
 d = {x:20,y:30}
-
-
-# In[18]:
-
 
 with tf.Session() as sess:
     print('Operations with Constants')
@@ -170,54 +107,20 @@ with tf.Session() as sess:
 
 # Now let's see an example of a more complex operation, using Matrix Multiplication. First we need to create the matrices:
 
-# In[19]:
-
-
 import numpy as np
 # Make sure to use floats here, int64 will cause an error.
 a = np.array([[5.0,5.0]])
 b = np.array([[2.0],[2.0]])
 
 
-# In[20]:
-
-
 a
-
-
-# In[21]:
-
-
 a.shape
-
-
-# In[22]:
-
-
 b
-
-
-# In[23]:
-
-
 b.shape
-
-
-# In[24]:
-
-
 mat1 = tf.constant(a)
 
 
-# In[25]:
-
-
 mat2 = tf.constant(b)
-
-
-# The matrix multiplication operation:
-
-# In[26]:
 
 
 matrix_multi = tf.matmul(mat1,mat2)
@@ -225,14 +128,6 @@ matrix_multi = tf.matmul(mat1,mat2)
 
 # Now run the session to perform the Operation:
 
-# In[27]:
-
-
 with tf.Session() as sess:
     result = sess.run(matrix_multi)
     print(result)
-
-
-# That is all for now! Next we will expand these basic concepts to construct out own Multi-Layer Perceptron model!
-
-# # Great Job!
