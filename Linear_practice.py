2ae6eae9d0e50d59378db74bb6c1bba61d6ebcc6
chunxu <41453752+chunxu@users.noreply.github.com>
2022-09-12 20:29:52 -0500

    Update Linear_practice.py

--- Linear_practice.py
+++ Linear_practice.py
@@ -17,8 +17,6 @@
 # ** Import pandas, numpy, matplotlib,and seaborn. Then set %matplotlib inline 
 # (You'll import sklearn as you need it.)**
 
-# In[275]:
-
 
 import pandas as pd
 import numpy as np
@@ -38,28 +36,15 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # 
 # ** Read in the Ecommerce Customers csv file as a DataFrame called customers.**
 
-# In[276]:
-
 
 customers = pd.read_csv("Ecommerce Customers")
 
 
 # **Check the head of customers, and check out its info() and describe() methods.**
-
-# In[277]:
-
-
 customers.head()
 
 
-# In[278]:
-
-
 customers.describe()
-
-
-# In[279]:
-
 
 customers.info()
 
@@ -72,15 +57,8 @@ customers.info()
 # ___
 # **Use seaborn to create a jointplot to compare the Time on Website and Yearly Amount Spent columns. Does the correlation make sense?**
 
-# In[280]:
-
-
 sns.set_palette("GnBu_d")
 sns.set_style('whitegrid')
-
-
-# In[281]:
-
 
 # More time on site, more money spent.
 sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=customers)
@@ -88,40 +66,27 @@ sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=customers)
 
 # ** Do the same but with the Time on App column instead. **
 
-# In[282]:
-
 
 sns.jointplot(x='Time on App',y='Yearly Amount Spent',data=customers)
 
 
 # ** Use jointplot to create a 2D hex bin plot comparing Time on App and Length of Membership.**
 
-# In[283]:
-
-
 sns.jointplot(x='Time on App',y='Length of Membership',kind='hex',data=customers)
 
 
 # **Let's explore these types of relationships across the entire data set. Use [pairplot](https://stanford.edu/~mwaskom/software/seaborn/tutorial/axis_grids.html#plotting-pairwise-relationships-with-pairgrid-and-pairplot) to recreate the plot below.(Don't worry about the the colors)**
-
-# In[284]:
-
 
 sns.pairplot(customers)
 
 
 # **Based off this plot what looks to be the most correlated feature with Yearly Amount Spent?**
 
-# In[285]:
-
 
 # Length of Membership 
 
 
 # **Create a linear model plot (using seaborn's lmplot) of  Yearly Amount Spent vs. Length of Membership. **
-
-# In[286]:
-
 
 sns.lmplot(x='Length of Membership',y='Yearly Amount Spent',data=customers)
 
@@ -131,13 +96,7 @@ sns.lmplot(x='Length of Membership',y='Yearly Amount Spent',data=customers)
 # Now that we've explored the data a bit, let's go ahead and split the data into training and testing sets.
 # ** Set a variable X equal to the numerical features of the customers and a variable y equal to the "Yearly Amount Spent" column. **
 
-# In[287]:
-
-
 y = customers['Yearly Amount Spent']
-
-
-# In[288]:
 
 
 X = customers[['Avg. Session Length', 'Time on App','Time on Website', 'Length of Membership']]
@@ -145,14 +104,8 @@ X = customers[['Avg. Session Length', 'Time on App','Time on Website', 'Length o
 
 # ** Use model_selection.train_test_split from sklearn to split the data into training and testing sets. Set test_size=0.3 and random_state=101**
 
-# In[289]:
-
 
 from sklearn.model_selection import train_test_split
-
-
-# In[290]:
-
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
 
@@ -163,31 +116,20 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 # 
 # ** Import LinearRegression from sklearn.linear_model **
 
-# In[291]:
-
-
 from sklearn.linear_model import LinearRegression
 
 
 # **Create an instance of a LinearRegression() model named lm.**
-
-# In[292]:
-
 
 lm = LinearRegression()
 
 
 # ** Train/fit lm on the training data.**
 
-# In[293]:
-
-
 lm.fit(X_train,y_train)
 
 
 # **Print out the coefficients of the model**
-
-# In[294]:
 
 
 # The coefficients
@@ -199,16 +141,10 @@ print('Coefficients: \n', lm.coef_)
 # 
 # ** Use lm.predict() to predict off the X_test set of the data.**
 
-# In[295]:
-
-
 predictions = lm.predict( X_test)
 
 
 # ** Create a scatterplot of the real test values versus the predicted values. **
-
-# In[296]:
-
 
 plt.scatter(y_test,predictions)
 plt.xlabel('Y Test')
@@ -220,9 +156,6 @@ plt.ylabel('Predicted Y')
 # Let's evaluate our model performance by calculating the residual sum of squares and the explained variance score (R^2).
 # 
 # ** Calculate the Mean Absolute Error, Mean Squared Error, and the Root Mean Squared Error. Refer to the lecture or to Wikipedia for the formulas**
-
-# In[303]:
-
 
 # calculate these metrics by hand!
 from sklearn import metrics
@@ -238,9 +171,6 @@ print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
 # 
 # **Plot a histogram of the residuals and make sure it looks normally distributed. Use either seaborn distplot, or just plt.hist().**
 
-# In[317]:
-
-
 sns.distplot((y_test-predictions),bins=50);
 
 
@@ -248,10 +178,6 @@ sns.distplot((y_test-predictions),bins=50);
 # We still want to figure out the answer to the original question, do we focus our efforst on mobile app or website development? Or maybe that doesn't even really matter, and Membership Time is what is really important.  Let's see if we can interpret the coefficients at all to get an idea.
 # 
 # ** Recreate the dataframe below. **
-
-# In[298]:
-
-
 coeffecients = pd.DataFrame(lm.coef_,X.columns)
 coeffecients.columns = ['Coeffecient']
 coeffecients
