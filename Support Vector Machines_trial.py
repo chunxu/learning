afe30a4f3116d5499c6de426c37505935b297fde
chunxu <41453752+chunxu@users.noreply.github.com>
2022-09-19 18:54:23 -0500

    Update Support Vector Machines_trial.py

--- Support Vector Machines_trial.py
+++ Support Vector Machines_trial.py
@@ -11,8 +11,6 @@
 # 
 # ## Import Libraries
 
-# In[1]:
-
 
 import pandas as pd
 import numpy as np
@@ -25,77 +23,38 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # 
 # We'll use the built in breast cancer dataset from Scikit Learn. We can get with the load function:
 
-# In[2]:
-
-
 from sklearn.datasets import load_breast_cancer
-
-
-# In[3]:
-
 
 cancer = load_breast_cancer()
 
 
 # The data set is presented in a dictionary form:
 
-# In[9]:
-
-
 cancer.keys()
 
-
-# In[12]:
-
-
 cancer['target_names']
-
-
-# In[55]:
-
 
 cancer.keys()
 
 
 # We can grab information and arrays out of this dictionary to set up our data frame and understanding of the features:
 
-# In[4]:
-
-
 print(cancer['DESCR'])
-
-
-# In[13]:
-
 
 cancer['feature_names']
 
 
 # ## Set up DataFrame
 
-# In[14]:
-
-
 df_feat = pd.DataFrame(cancer['data'],columns=cancer['feature_names'])
 df_feat.info()
 
-
-# In[14]:
-
-
 cancer['target']
-
-
-# In[18]:
-
 
 df_target = pd.DataFrame(cancer['target'],columns=['Cancer'])
 
 
 # Now let's actually check out the dataframe!
-
-# In[32]:
-
 
 df_target[df_target['Cancer']==0].count()
 
@@ -108,35 +67,17 @@ df_target[df_target['Cancer']==0].count()
 
 # ## Train Test Split
 
-# In[20]:
-
-
 from sklearn.model_selection import train_test_split
-
-
-# In[21]:
-
 
 X_train, X_test, y_train, y_test = train_test_split(df_feat, np.ravel(df_target), test_size=0.30, random_state=101)
 
 
 # # Train the Support Vector Classifier
 
-# In[22]:
-
 
 from sklearn.svm import SVC
 
-
-# In[25]:
-
-
 model = SVC()
-
-
-# In[26]:
-
-
 model.fit(X_train,y_train)
 
 
@@ -144,31 +85,13 @@ model.fit(X_train,y_train)
 # 
 # Now let's predict using the trained model.
 
-# In[27]:
-
-
 predictions = model.predict(X_test)
-
-
-# In[28]:
-
 
 from sklearn.metrics import classification_report,confusion_matrix
 
-
-# In[29]:
-
-
 print(confusion_matrix(y_test,predictions))
 
-
-# In[30]:
-
-
 print(classification_report(y_test,predictions))
-
-
-# In[ ]:
 
 
 SVC()
@@ -184,30 +107,17 @@ SVC()
 # 
 # GridSearchCV takes a dictionary that describes the parameters that should be tried and a model to train. The grid of parameters is defined as a dictionary, where the keys are the parameters and the values are the settings to be tested. 
 
-# In[33]:
-
-
 param_grid = {'C': [0.1,1, 10, 100, 1000], 'gamma': [1,0.1,0.01,0.001,0.0001], 'kernel': ['rbf']} 
-
-
-# In[34]:
-
 
 from sklearn.model_selection import GridSearchCV
 
 
 # One of the great things about GridSearchCV is that it is a meta-estimator. It takes an estimator like SVC, and creates a new estimator, that behaves exactly the same - in this case, like a classifier. You should add refit=True and choose verbose to whatever number you want, higher the number, the more verbose (verbose just means the text output describing the process).
 
-# In[35]:
-
-
 grid = GridSearchCV(SVC(),param_grid,refit=True,verbose=3)
 
 
 # What fit does is a bit more involved then usual. First, it runs the same loop with cross-validation, to find the best parameter combination. Once it has the best combination, it runs fit again on all data passed to fit (without cross-validation), to built a single new model using the best parameter setting.
-
-# In[36]:
-
 
 # May take awhile!
 grid.fit(X_train,y_train)
@@ -215,13 +125,8 @@ grid.fit(X_train,y_train)
 
 # You can inspect the best parameters found by GridSearchCV in the best_params_ attribute, and the best estimator in the best\_estimator_ attribute:
 
-# In[37]:
-
-
 grid.best_params_
 
-
-# In[38]:
 
 
 grid.best_estimator_
@@ -229,19 +134,11 @@ grid.best_estimator_
 
 # Then you can re-run predictions on this grid object just like you would with a normal model.
 
-# In[39]:
-
 
 grid_predictions = grid.predict(X_test)
 
 
-# In[40]:
-
-
 print(confusion_matrix(y_test,grid_predictions))
-
-
-# In[41]:
 
 
 print(classification_report(y_test,grid_predictions))
