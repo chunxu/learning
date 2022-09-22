d5812c3a9c1c79b5fa608c1ec3a26b9dc2bf455f
chunxu <41453752+chunxu@users.noreply.github.com>
2022-09-21 21:22:26 -0500

    Update K Means Clustering_trial.py

--- K Means Clustering_trial.py
+++ K Means Clustering_trial.py
@@ -16,8 +16,6 @@
 
 # ## Import Libraries
 
-# In[22]:
-
 
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -26,14 +24,8 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 # ## Create some Data
 
-# In[23]:
-
 
 from sklearn.datasets import make_blobs
-
-
-# In[42]:
-
 
 # Create Data
 data = make_blobs(n_samples=200, n_features=2, 
@@ -41,47 +33,20 @@ data = make_blobs(n_samples=200, n_features=2,
 
 
 # ## Visualize Data
-
-# In[43]:
-
-
 plt.scatter(data[0][:,0],data[0][:,1],c=data[1],cmap='rainbow')
 
 
 # ## Creating the Clusters
 
-# In[48]:
-
-
 from sklearn.cluster import KMeans
-
-
-# In[49]:
-
 
 kmeans = KMeans(n_clusters=4)
 
-
-# In[50]:
-
-
 kmeans.fit(data[0])
-
-
-# In[51]:
-
 
 kmeans.cluster_centers_
 
-
-# In[55]:
-
-
 kmeans.labels_
-
-
-# In[69]:
-
 
 f, (ax1, ax2) = plt.subplots(1, 2, sharey=True,figsize=(10,6))
 ax1.set_title('K Means')
